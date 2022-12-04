79cc56e0f08dde9ba4f36e38d167ad400b5659f6
humoro <lijin1905@163.com>
2022-12-04 14:33:35 +0800

    [feat]clear master

--- MyCalendar/app/src/main/java/com/example/mycalendar/adapters/FilterListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.mycalendar.adapters;
-
-import com.example.mycalendar.Schedule;
-
-import java.util.List;
-
-public interface FilterListener {
-        void getFilterData(List<Schedule> list);// 获取过滤后的数据
-}
